104900a0619317e6eb7a9920846fac9439a7e105
lucas-ch <lchaigne@live.fr>
2020-08-15 15:04:56 +0200

    #1 add unit tests for ValidationService
    unit tests have been added for ValidationService and case when there is
    no compatibility matrix is covered

--- bundles/fr.adaussy.permadeler.model.design/src/fr/adaussy/permadeler/model/design/services/ValidationService.java
+++ bundles/fr.adaussy.permadeler.model.design/src/fr/adaussy/permadeler/model/design/services/ValidationService.java
@@ -12,66 +12,53 @@ package fr.adaussy.permadeler.model.design.services;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.eclipse.emf.common.util.EList;
-
 import fr.adaussy.permadeler.model.Permadeler.Area;
 import fr.adaussy.permadeler.model.Permadeler.CompatibilityLink;
 import fr.adaussy.permadeler.model.Permadeler.Genus;
 import fr.adaussy.permadeler.model.Permadeler.Root;
 import fr.adaussy.permadeler.model.utils.EMFUtils;
 
+/**
+ * ValidationService: return true if all the plantations in an area are compatible
+ * 
+ * @param Area
+ *            the area to be validated
+ */
 public class ValidationService {
 
 	public static boolean validatePlantationCompatibility(Area self) {
 
-		// get the list of incompatible species
-
-		// session = SessionManager.INSTANCE.getSession(self).getSemanticCrossReferencer()
-
 		Root root = EMFUtils.getAncestor(Root.class, self);
-		EList<CompatibilityLink> compatibilityLinkAll = root.getSeedLib().getCompatibilityMatrix()
-				.getCompatibilties();
 
 		Set<Genus> plantedGenus = self.getPlantations().stream()//
 				.filter(e -> e.getType() != null && e.getType().getType() != null)//
 				.map(e -> e.getType().getType())//
 				.collect(Collectors.toSet());
 
-		return !root.getSeedLib().getCompatibilityMatrix().getCompatibilties().stream().anyMatch(l -> {
-			return isIncompatibleLink(plantedGenus, l);
-		});
-		// Set<CompatibilityLink> compatibilityLinkNeg = new HashSet<>();
-		// for (CompatibilityLink e : compatibilityLinkAll) {
-		// if (e.getAffinity() < 0) {
-		// compatibilityLinkNeg.add(e);
-		// }
-		// }
-		//
-		// // get the list of plantations in area
-		// EList<Plantation> plantations = self.getPlantations();
-		//
-		// Set<Genus> plantedGenus = new HashSet<>();
-		// for (Plantation e : plantations) {
-		// Genus genus = e.getType().getType();
-		// plantedGenus.add(genus);
-		// }
-		//
-		// // get the list of incompatible genus
-		// for (CompatibilityLink e : compatibilityLinkNeg) {
-		//
-		// Set<Genus> incompatGenuses = new HashSet<>(e.getSpecies());
-		//
-		// if (Sets.intersection(incompatGenuses, plantedGenus).size() >= 2) {
-		// System.out.println("Incompatible stuff");
-		// return false;
-		// }
-		// }
-		//
-		// System.out.println("Everything ok");
-		// return true;
+		if (root.getSeedLib() != null && root.getSeedLib().getCompatibilityMatrix() != null
+				&& root.getSeedLib().getCompatibilityMatrix().getCompatibilties() != null) {
+
+			return !root.getSeedLib().getCompatibilityMatrix().getCompatibilties().stream()//
+					.anyMatch(l -> {
+						return isIncompatibleLink(plantedGenus, l);
+					});
+
+		} else {
+			return true;
+
+		}
 
 	}
 
+	/**
+	 * isIncompatibleLink: return true if Genus in a set of Genus are incompatible according to a
+	 * CompatibilityLink
+	 * 
+	 * @param plantedGenus
+	 *            a set of Genus
+	 * @param l
+	 *            a CompatibilityLink
+	 */
 	private static boolean isIncompatibleLink(Set<Genus> plantedGenus, CompatibilityLink l) {
 		return l.getAffinity() < 0
 				&& l.getSpecies().stream().filter(g -> plantedGenus.contains(g)).count() >= 2;
