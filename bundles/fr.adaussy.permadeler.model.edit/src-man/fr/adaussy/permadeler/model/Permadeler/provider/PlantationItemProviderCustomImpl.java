4345e564f2839e3aafa6be9e3fb7755e2486fe58
Arthur Daussy <arthur.daussy@obeo.fr>
2021-01-20 15:42:07 +0100

    Fix labels

--- bundles/fr.adaussy.permadeler.model.edit/src-man/fr/adaussy/permadeler/model/Permadeler/provider/PlantationItemProviderCustomImpl.java
+++ bundles/fr.adaussy.permadeler.model.edit/src-man/fr/adaussy/permadeler/model/Permadeler/provider/PlantationItemProviderCustomImpl.java
@@ -10,9 +10,15 @@
  */
 package fr.adaussy.permadeler.model.Permadeler.provider;
 
-import org.eclipse.emf.common.notify.AdapterFactory;
-import org.eclipse.emf.ecore.EObject;
+import java.time.Instant;
 
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import fr.adaussy.permadeler.model.Permadeler.Event;
+import fr.adaussy.permadeler.model.Permadeler.PermadelerPackage;
 import fr.adaussy.permadeler.model.Permadeler.Plantation;
 import fr.adaussy.permadeler.model.Permadeler.Species;
 import fr.adaussy.permadeler.model.edit.ImageProvider;
@@ -36,10 +42,37 @@ public class PlantationItemProviderCustomImpl extends PlantationItemProvider {
 
 	@Override
 	public String getText(Object object) {
-		Species type = ((Plantation)object).getType();
+		Plantation plantation = (Plantation)object;
+		Species type = plantation.getType();
+		String label;
 		if (type != null) {
-			return TextHelper.getLabel(type);
+			label = TextHelper.getLabel(type);
+		} else {
+			label = "Plantation with no species";
 		}
-		return "Plantation with no species";
+
+		Event rEvent = plantation.getRemovalEvent();
+		if (rEvent != null) {
+			if (rEvent.getDate().compareTo(Instant.now()) < 0) {
+				label = "[Removed] " + label;
+			}
+		}
+
+		return label;
 	}
+
+	@Override
+	public void notifyChanged(Notification notification) {
+		super.notifyChanged(notification);
+
+		switch (notification.getFeatureID(Plantation.class)) {
+			case PermadelerPackage.PLANTATION__EVENTS:
+			case PermadelerPackage.PLANTATION__REMOVAL_EVENT:
+				fireNotifyChanged(
+						new ViewerNotification(notification, notification.getNotifier(), true, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
 }
