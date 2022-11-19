cdae6b06eaa6aa6c35d441fdd456d511b88906b7
Arthur Daussy <arthur.daussy@obeo.fr>
2022-11-19 17:59:10 +0100

    Add missing header

--- bundles/fr.adaussy.permadeler.rcp/src/fr/adaussy/permadeler/rcp/internal/handlers/SowCalendarSeedBankDirectMenu.java
+++ bundles/fr.adaussy.permadeler.rcp/src/fr/adaussy/permadeler/rcp/internal/handlers/SowCalendarSeedBankDirectMenu.java
@@ -1,4 +1,12 @@
-
+/*******************************************************************************
+ * Copyright (c) 2022 Arthur Daussy.
+ *
+ * This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 
+ * which is available at https://www.eclipse.org/legal/epl-2.0/ 
+ * Contributors:
+ * Arthur Daussy - initial API and implementation.
+ ******************************************************************************/
 package fr.adaussy.permadeler.rcp.internal.handlers;
 
 import fr.adaussy.permadeler.model.Permadeler.SeedBank;
