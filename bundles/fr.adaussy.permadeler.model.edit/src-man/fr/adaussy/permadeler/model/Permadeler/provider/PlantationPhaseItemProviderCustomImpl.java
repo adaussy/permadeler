01fcba80467e248d44229068ee632b70f046323c
Arthur Daussy <arthur.daussy@gmail.com>
2023-01-31 08:05:22 +0100

    Prevents displaying BackgroundImage in Tree

--- bundles/fr.adaussy.permadeler.model.edit/src-man/fr/adaussy/permadeler/model/Permadeler/provider/PlantationPhaseItemProviderCustomImpl.java
+++ bundles/fr.adaussy.permadeler.model.edit/src-man/fr/adaussy/permadeler/model/Permadeler/provider/PlantationPhaseItemProviderCustomImpl.java
@@ -1,8 +1,12 @@
 package fr.adaussy.permadeler.model.Permadeler.provider;
 
+import java.util.Collection;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
+import fr.adaussy.permadeler.model.Permadeler.PermadelerPackage;
 import fr.adaussy.permadeler.model.Permadeler.PlantationPhase;
 import fr.adaussy.permadeler.model.edit.ImageProvider;
 
@@ -20,6 +24,13 @@ public class PlantationPhaseItemProviderCustomImpl extends PlantationPhaseItemPr
 	@Override
 	public String getText(Object object) {
 		return ((PlantationPhase)object).getName();
+	}
+
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		Collection<? extends EStructuralFeature> result = super.getChildrenFeatures(object);
+		result.remove(PermadelerPackage.Literals.PLANTATION_PHASE__BACKGROUND_IMAGE);
+		return result;
 	}
 
 }
