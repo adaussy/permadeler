1bd6f541ae67e6ed37bb7c0b680610c0c9c34daa
Arthur Daussy <arthur.daussy@gmail.com>
2023-11-03 19:52:42 +0100

    Fix DND on existing plantation

--- bundles/fr.adaussy.permadeler.model.design/src/fr/adaussy/permadeler/model/design/editparts/PlantationEditPart.java
+++ bundles/fr.adaussy.permadeler.model.design/src/fr/adaussy/permadeler/model/design/editparts/PlantationEditPart.java
@@ -3,6 +3,7 @@ package fr.adaussy.permadeler.model.design.editparts;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
+import org.eclipse.gef.requests.ChangeBoundsRequest;
 import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.sirius.diagram.DNode;
@@ -20,17 +21,13 @@ public class PlantationEditPart extends DNodeEditPart {
 	public EditPart getTargetEditPart(Request request) {
 		if (request instanceof CreateRequest && ((CreateRequest)request).getNewObject() != null) {
 			// When the plantation is in wireframe mode then the creation request sould be sent to the parent
-			if (isWireframePlantation()) {
-				return getParent();
-			}
+			return getParent().getTargetEditPart(request);
+		}
+		// Dropping on plantation should be the same of dropping he diagram itself
+		else if (request instanceof ChangeBoundsRequest chReq && "drop".equals(chReq.getType())) {
+			return getParent().getTargetEditPart(request);
 		}
 		return super.getTargetEditPart(request);
-	}
-
-	//
-	private boolean isWireframePlantation() {
-		Plantation plantation = getPlantation();
-		return plantation != null && plantation.isWireframe();
 	}
 
 	private Plantation getPlantation() {
